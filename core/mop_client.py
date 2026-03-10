54a2892f418830b90106df0d083f1d8dfa5ded48
firthunand <fdo.delabarra@gmail.com>
2026-03-10 16:16:20 -0300

    feat: renderizacion chrome noVNC

--- core/mop_client.py
+++ core/mop_client.py
@@ -111,6 +111,12 @@ def run_download_job(
     chrome_options.add_argument("--headless=new")
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_options.add_argument("--disable-gpu")
+    chrome_options.add_argument("--window-size=1920,1080")
+    chrome_options.add_argument("--remote-debugging-port=9222")
+    chrome_options.add_argument("driver.set_page_load_timeout(600)")
     ############################################################
     
     driver = webdriver.Chrome(options=chrome_options)
